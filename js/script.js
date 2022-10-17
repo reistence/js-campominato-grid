ae79d17f6f63f08beb53f35d2d12e2beb43378fb
reistence <gvlamberti@gmail.com>
2022-10-17 17:16:06 +0200

    difficulties implemented

--- js/script.js
+++ js/script.js
@@ -1,48 +1,75 @@
-// **Consegna**
-// L'utente clicca su un bottone che genererà una griglia
-// di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata
-// si colora di azzurro ed emetto un messaggio in console
-// con il numero della cella cliccata.
-// **Bonus**
-// Aggiungere una select accanto al bottone di generazione,
-// che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso
-// tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso
-// tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso
-// tra 1 e 49, divise in 7 caselle per 7 righe;
-
 const playBtn = document.getElementById("play");
 const row = document.querySelector(".row");
-let squaresNr = 100;
-console.log(squaresNr, typeof squaresNr);
 
-// generate an array of given number size
-const numbersArray = [];
-console.log(numbersArray);
+// play btn
+playBtn.addEventListener("click", function () {
+  let difficulty = document.getElementById("difficulty").value;
+  console.log(difficulty, typeof difficulty);
 
-// for (i = 1; i <= squaresNr; i++) {
-//   console.log(i);
-//   numbersArray.push(i);
-//   const square = document.createElement("div");
-//   square.classList.add("square");
-//   square.classList.add("easy");
-//   square.innerHTML = i;
-//   console.log(square);
-//   row.append(square);
-// }
-
-generateOrderedArray(squaresNr);
-console.log(numbersArray);
-
-// FUNCTION
-function generateOrderedArray(arrayLength) {
-  const numbersArray = [];
-  for (i = 1; i <= arrayLength; i++) {
-    numbersArray.push(i);
+  let squaresNr;
+  if (difficulty === "easy") {
+    squaresNr = 100;
+  } else if (difficulty === "medium") {
+    squaresNr = 81;
+  } else if (difficulty === "difficult") {
+    squaresNr = 49;
   }
-  return numbersArray;
+
+  // generate an array of given number size
+  let numbersArray = [];
+
+  numbersArray = generateOrderedArray(squaresNr);
+
+  for (let i = 0; i < numbersArray.length; i++) {
+    const thisNr = numbersArray[i];
+    const thisSquare = createSquare(thisNr);
+    thisSquare.addEventListener("click", SquareClickMessage);
+    row.append(thisSquare);
+    if (difficulty === "easy") {
+      thisSquare.classList.add("easy");
+    } else if (difficulty === "medium") {
+      thisSquare.classList.remove("easy");
+      thisSquare.classList.add("medium");
+    } else if (difficulty === "difficult") {
+      thisSquare.classList.add("difficult");
+    }
+  }
+});
+console.log();
+
+// FUNCTIONs
+/**
+ * Description generate an array of ascending numbers from 1 to a given lenght
+ * @param {number} arrayLength
+ * @returns {array}
+ */
+function generateOrderedArray(arrayLength) {
+  const array = [];
+  for (let i = 1; i <= arrayLength; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+/**
+ * Description create a div with the class .square and put a certain number in it
+ * @param {number} innerNumber
+ * @returns {Element} Square
+ */
+function createSquare(innerNumber) {
+  const newSquare = document.createElement("div");
+  newSquare.classList.add("square");
+  newSquare.innerHTML = innerNumber;
+  return newSquare;
+}
+
+/**
+ * Description returns the innerhtml content (as a number) of a given element
+ * and changes its background color to lightblue in the DOM
+ * @returns {number}
+ */
+function SquareClickMessage() {
+  const clickedNr = parseInt(this.innerHTML);
+  console.log(clickedNr);
+  this.classList.add("active");
 }
